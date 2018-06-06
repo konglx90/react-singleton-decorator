9d7032f262e66d14c4b40a7f84715fe3973576ce
konglx <konglx90@gmail.com>
2018-06-06 19:22:19 +0800

    fix

--- examples/Hello.js
+++ examples/Hello.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
 
+function log(target, name, descriptor) {
+    let message = `LOG: Calling \`${name}\` function.`;
+  
+    if (typeof target === 'string') {
+      message = target;
+  
+      return (target, name, descriptor) => {
+        console.log(`LOG: ${message}`, name);
+        return descriptor;
+      };
+    } else {
+      console.log(message);
+      return descriptor;
+    }
+  }
+
+@log
+function x() {
+    console.log('x');
+}
+
+x();
+
 export default class Hello extends Component {
+    @log
+    x() {
+        console.log('xx');
+    }
+
     render() {
+        this.x();
         return (
             <div>
                 hello world!
